a4517edffaa331f539ad3a61383e41163fb76609
sean <maherseanmilo@gmail.com>
2021-01-15 10:32:42 -0600

    adds routes for single and all movies with appropriate views

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,2 +1,13 @@
 class Api::MoviesController < ApplicationController
+
+  def all_movies_action
+    @movies = Movie.all
+    render 'all_movies.json.jb'
+  end
+
+  def single_movie_action
+    @movie = Movie.first
+    render 'single_movie.json.jb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get '/actors_path' => 'actors#single_actor_display_action'
+    get '/all_movies_path' => 'movies#all_movies_action'
+    get '/single_movie_path' => 'movies#single_movie_action'
   end
 end
