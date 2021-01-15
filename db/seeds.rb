53e477d60857e7d59a11c1f94e04c22fcaf99672
sean <maherseanmilo@gmail.com>
2021-01-15 10:20:14 -0600

    adds several instances of Movie and creates a controller api/movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Actor.create(
+Movie.create(
   [
     {
-      first_name: "Benedict",
-      last_name: "Cumberbatch",
-      known_for: "Sherlock"
+      title: "The Matrix",
+      year: 1999,
+      plot: "A man finds out that his world isn't what he thinks it is."
+    },
+    {
+      title: "Pineapple Express",
+      year: 2008,
+      plot: "A stoner witnesses a murder. He and his stoner friend try to get out alive while the murderers try to get em."
+    },
+    {
+      title: "JoJo Rabbit",
+      year: 2017,
+      plot: "A little boy and his imaginary friend, Adolf Hitler, do their best to fit in amongst the Hitler Youth."
     }
   ]
 )
