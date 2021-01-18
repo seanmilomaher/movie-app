2c5f10af5c6f9a66044600cd3c6bce82dc22b752
sean <maherseanmilo@gmail.com>
2021-01-18 10:04:03 -0600

    adds search parameters to acotr routes and controller

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,7 +1,8 @@
 class Api::ActorsController < ApplicationController
 
   def single_actor_display_action
-    @actor = Actor.find_by(id: 6)
+    actor_id = params[:actor].to_i
+    @actor = Actor.find_by(id: actor_id)
     render 'single_actor_display.json.jb'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get '/actors_path' => 'actors#single_actor_display_action'
+    get '/actor_path/:actor' => 'actors#single_actor_display_action'
     get '/all_movies_path' => 'movies#all_movies_action'
     get '/single_movie_path' => 'movies#single_movie_action'
   end
