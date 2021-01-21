814fa7deb22bcda3f375c899557a9123caa97509
sean <maherseanmilo@gmail.com>
2021-01-21 10:06:01 -0600

    debugss restuful controllers

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -28,8 +28,8 @@ class Api::MoviesController < ApplicationController
     @movie.title = params[:title] || @movie.title
     @movie.year = params[:year] || @movie.year
     @movie.plot = params[:plot] || @movie.plot
-    @movie.director = params[:director] || movie.director
-    @movie.english = params[:english] || movie.english
+    @movie.director = params[:director] || @movie.director
+    @movie.english = params[:english] || @movie.english = true
     @movie.save
     render "show.json.jb"
   end
