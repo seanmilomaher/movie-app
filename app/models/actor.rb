7acf8e73225af04ac9ffb6942eb1c4abb054c2b5
sean <maherseanmilo@gmail.com>
2021-01-22 09:19:21 -0600

    adds validations for the Acotr model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,7 @@
 class Actor < ApplicationRecord
+
+  validates :first_name, :last_name, length: { minimum: 2 }
+  validates :known_for, presence: true
+  validates :age, numericality: { greater_than_or_equal_to: 13 }
+
 end
