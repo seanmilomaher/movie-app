b8836360dcecb39c16c4d39063f4398e6149fb05
sean <maherseanmilo@gmail.com>
2021-02-01 08:43:16 -0700

    adds Genre and MovieGenre with associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ApplicationRecord
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 
   # validates :title, :director, presence: true
   # validates :year, length: { is: 4 }
