87583b9a335047dd67f8dac728f841dc43b73c12
sean <maherseanmilo@gmail.com>
2021-02-17 09:40:44 -0600

    fixes movies index action

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -2,7 +2,7 @@ class Api::MoviesController < ApplicationController
 
   before_action :authenticate_admin, except: [:index, :show]
 
-  def inde
+  def index
     @movies = Movie.where(english: :true)
     render 'index.json.jb'
   end
